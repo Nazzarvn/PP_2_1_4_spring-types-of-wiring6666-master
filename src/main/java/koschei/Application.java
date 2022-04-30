4e8ee0cb2af2cd6387ddc6f6895b7e69a5a85114
Japar <95058287+Nazzarvn@users.noreply.github.com>
2022-05-01 01:28:18 +0300

    Initial commit

---
+++ src/main/java/koschei/Application.java
@@ -0,0 +1,21 @@
+package koschei;
+
+import koschei.config.AppConfig;
+import koschei.models.Ocean1;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Application {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        KoscheiTheDeathless koscheiTheDeathless =
+                applicationContext.getBean(KoscheiTheDeathless.class);
+        System.out.println(koscheiTheDeathless.getRulesByDeth());
+
+
+
+
+    }
+}
---
+++ src/main/java/koschei/KoscheiTheDeathless.java
@@ -0,0 +1,26 @@
+package koschei;
+
+import koschei.models.Island2;
+import koschei.models.Ocean1;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@Component
+public class KoscheiTheDeathless {
+
+    private Ocean1 ocean;
+
+    public String getRulesByDeth() {
+        return "На свете есть океан , " + ocean.toString();
+    }
+
+    @Autowired
+    public void setOcean(Ocean1 ocean) {
+        this.ocean = ocean;
+    }
+
+
+}
